714d9106a952f3963436d1eb736b335945e4c4df
yuta <y_kimura_y@r.recruit.co.jp>
2019-10-21 11:31:15 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [name, setName] = React.useState("")
+  const [email, setEmail] = React.useState("")
+  const [submitted, changeStatus] = React.useState(false)
+  const content = submitted ? (
+    <p>
+      Thanks for joining in! <br />
+      When we're ready to wow you, <br />
+      You'll get an email.
+    </p>
+  ) : (
+    <form>
+      <p>
+        A social network, <br />
+        Where you are the customer. <br />
+        Ad free. Launching soon.
+      </p>
+      <label>
+        Name:
+        <input value={name} onChange={(e) => setName(e.target.value)}/>
+      </label>
+      <br />
+      <label>
+        Email:
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit" onClick={() => changeStatus(true)}>
+        I'll vouch for that
+      </button>
+    </form>
+  )
+
+  return <div className="App">{content}</div>
 }
 
 export default App;
